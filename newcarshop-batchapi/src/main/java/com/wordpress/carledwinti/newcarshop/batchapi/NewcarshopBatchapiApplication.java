df5c2b09d2e67cb796223a2a7767eb6c47579807
demisgps <demisgps@gmail.com>
2022-09-29 09:36:43 -0300

    finalizando projeto

--- newcarshop-batchapi/src/main/java/com/wordpress/carledwinti/newcarshop/batchapi/NewcarshopBatchapiApplication.java
+++ newcarshop-batchapi/src/main/java/com/wordpress/carledwinti/newcarshop/batchapi/NewcarshopBatchapiApplication.java
@@ -2,8 +2,10 @@ package com.wordpress.carledwinti.newcarshop.batchapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class NewcarshopBatchapiApplication {
 
 	public static void main(String[] args) {
